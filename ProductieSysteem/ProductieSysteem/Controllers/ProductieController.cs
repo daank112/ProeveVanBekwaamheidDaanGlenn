b5995bd0971900cfc97891524ccdd3056a0147a5
glennooms <D182159@edu.rocwb.com>
2015-05-19 13:26:46 +0200

    productie
    add a controller for product
    add a view for product

---
+++ ProductieSysteem/ProductieSysteem/Controllers/ProductieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductieSysteem.Models;
+
+
+namespace ProductieSysteem.Controllers
+{
+    public class ProductieController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductieOpgeven()
+        {
+            return View();
+        }
+        public ActionResult ProductieBijstellen()
+        {
+            return View();
+        }
+    }
+}
